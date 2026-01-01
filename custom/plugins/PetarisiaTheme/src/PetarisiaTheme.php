bb4925db58c1bf309e57253cc48ef365ca58fa0e
walid1921 <kouiderayadwalid@gmail.com>
2026-01-01 19:17:08 +0100

    Initial commit

---
+++ custom/plugins/PetarisiaTheme/src/PetarisiaTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace PetarisiaTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class PetarisiaTheme extends Plugin implements ThemeInterface
+{
+}
